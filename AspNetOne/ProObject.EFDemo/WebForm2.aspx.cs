37a8f3d30d3e82c6d32697c847d0029001455e60
bzy734084360@hotmail.com <0000>
2024-06-07 22:12:18 +0800

    二周目 EF学习

--- AspNetOne/ProObject.EFDemo/WebForm2.aspx.cs
+++ AspNetOne/ProObject.EFDemo/WebForm2.aspx.cs
@@ -13,39 +13,53 @@ namespace ProObject.EFDemo
         {
 
         }
-
+        /// <summary>
+        /// 新增数据，导航属性关联新增
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Model2Container db = new Model2Container();
+            ModelFirstContainer db = new ModelFirstContainer();
             Customer customer = new Customer() { CustomerName = "张三", CustomerPwd = "123", SubTime = DateTime.Now };
             OrderInfo orderInfo1 = new OrderInfo() { Id = Guid.NewGuid(), OrderNum = "10001", CreateDateTime = DateTime.Now, Customer = customer };
             OrderInfo orderInfo2 = new OrderInfo() { Id = Guid.NewGuid(), OrderNum = "10002", CreateDateTime = DateTime.Now, Customer = customer };
+            //先新增客户 否则订单无法拿到用户ID
             db.Customer.Add(customer);
             db.OrderInfo.Add(orderInfo1);
             db.OrderInfo.Add(orderInfo2);
+            //默认开启事务
             db.SaveChanges();
         }
-
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Button2_Click(object sender, EventArgs e)
         {
             //导航属性应用
-            Model2Container db = new Model2Container();
+            ModelFirstContainer db = new ModelFirstContainer();
             var customerList = from c in db.Customer
                                select c;
             foreach (var item in customerList)
             {
                 //打印名字及他用户下的订单号
                 Response.Write(item.CustomerName + ":");
-                foreach (var orderInfo in item.OrderInfo)
+                foreach (var orderInfo in item.OrderInfo)//延迟加载
                 {
                     Response.Write(orderInfo.OrderNum);
                 }
             }
         }
-
+        /// <summary>
+        /// 查询某个人的订单
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Button3_Click(object sender, EventArgs e)
         {
-            Model2Container db = new Model2Container();
+            ModelFirstContainer db = new ModelFirstContainer();
             //常规方式
             //var customerInfoList = from c in db.Customer
             //                       where c.Id == 1
@@ -64,21 +78,29 @@ namespace ProObject.EFDemo
                 Response.Write(item.OrderNum);
             }
         }
-
+        /// <summary>
+        /// 根据订单查询到用户信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Button4_Click(object sender, EventArgs e)
         {
-            Model2Container db = new Model2Container();
+            ModelFirstContainer db = new ModelFirstContainer();
             var orderInfoList = from c in db.OrderInfo
                                 where c.OrderNum == "10001"
                                 select c;
             var orderInfo = orderInfoList.FirstOrDefault();
             Response.Write(orderInfo.Customer.CustomerName);
         }
-
+        /// <summary>
+        /// 删除某个客户下的所有订单
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Button5_Click(object sender, EventArgs e)
         {
-            Model2Container db = new Model2Container();
-            //方式1
+            ModelFirstContainer db = new ModelFirstContainer();
+            //方式1 查询用户信息时，获取订单信息，标记订单信息进行删除
             //var customer = (from c in db.Customer
             //                where c.Id == 1
             //                select c).FirstOrDefault();
@@ -90,6 +112,7 @@ namespace ProObject.EFDemo
             //    db.Entry(orderInfo).State = System.Data.Entity.EntityState.Deleted;
             //}
             //db.SaveChanges();
+            //方式2 根据用户外键 标记订单信息
             var orderList = from o in db.OrderInfo
                             where o.CustomerId == 4
                             select o;
